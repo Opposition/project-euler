202067f7c613ea94ec545479b41a68795b323a29
Daniel Kaypour <danielkay14@hotmail.com>
2016-04-15 03:25:26 +0200

    Solved problem 48

---
+++ go/p048.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+/*
+
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+*/
+
+func pow(n, p int) *big.Int {
+	constNum := big.NewInt(int64(n))
+	result := big.NewInt(1)
+	for i := 0; i < p; i++ {
+		result.Mul(result, constNum)
+	}
+	return result
+}
+
+func main() {
+	result := new(big.Int)
+	for i := 1; i <= 1000; i++ {
+		result.Add(result, pow(i, i))
+	}
+	fmt.Println(result.String()[len(result.String())-10:]) //outputs: 9110846700
+}
